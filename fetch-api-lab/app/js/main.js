974afcddc299c12b8a70ed720ac24251f18870ea
Rosit1712 <sanuuusirosyid@gmail.com>
2020-04-18 05:40:33 +0700

    fetch-api

---
+++ fetch-api-lab/app/js/main.js
@@ -0,0 +1,156 @@
+/*
+Copyright 2018 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+// helper functions ----------
+
+function logResult(result) {
+  console.log(result);
+}
+
+function logError(error) {
+  console.log('Looks like there was a problem:', error);
+}
+
+
+// Fetch JSON ----------
+
+function fetchJSON() {
+  fetch('examples/animals.json')
+  // fetch('examples/non-existent.json')
+    .then(validateResponse) //check response validity
+    .then(logResult) //2
+    .then(readResponseAsJSON ) //3
+    .catch(logError);
+}
+const jsonButton = document.getElementById('json-btn');
+jsonButton.addEventListener('click', fetchJSON);
+
+
+// Fetch Image ----------
+
+function fetchImage() {
+  fetch('examples/fetching.jpg')
+    .then(validateResponse)
+    .then(readResponseAsBlob)
+    .then(showImage)
+    .catch(logError);
+}
+const imgButton = document.getElementById('img-btn');
+imgButton.addEventListener('click', fetchImage);
+
+
+// Fetch text ----------
+
+function fetchText() {
+  fetch('examples/words.txt')
+    .then(validateResponse)
+    .then(readResponseAsText)
+    .then(showText)
+    .catch(logError);
+}
+const textButton = document.getElementById('text-btn');
+textButton.addEventListener('click', fetchText);
+
+
+// HEAD request ----------
+
+function headRequest() {
+  fetch('examples/words.txt', {
+    method: 'HEAD'
+  })
+    .then(validateResponse)
+    .then(readResponseAsText)
+    .then(logResult)
+    .catch(logError);
+}
+const headButton = document.getElementById('head-btn');
+headButton.addEventListener('click', headRequest);
+
+
+// POST request ----------
+
+/* NOTE: Never send unencrypted user credentials in production! */
+function postRequest() {
+  // fetch('http://localhost:5000', {
+  //   method: 'POST', 
+  //   body: 'name=david&message=hello'
+  // })
+  // .then(validateResponse)
+  // .then(readResponseAsText)
+  // .then(showText)
+  // .catch(logError);
+  // const formData = new FormData(document.getElementById('msg-form'));
+  // fetch('http://localhost:5001/', {
+  //   method: 'POST', 
+  //   body: formData,
+  //   mode: 'no-cors'
+  // })
+  // .then(logResult)
+  // .catch(logError);
+  const formData = new FormData(document.getElementById('msg-form'));
+  fetch('http://localhost:5000/', {
+    method: 'POST', 
+    body: formData,
+  })
+  .then(validateResponse)
+  .then(readResponseAsText)
+  .then(showText)
+  .catch(logError);
+}
+const postButton = document.getElementById('post-btn');
+postButton.addEventListener('click', postRequest);
+
+// Check response validity
+function validateResponse(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
+
+// Read the response
+function readResponseAsJSON(response) {
+  return response.json();
+}
+
+//fetch image
+function showImage(responseAsBlob) {
+  const container = document.getElementById('img-container');
+  const imgElem = document.createElement('img')
+  container.appendChild(imgElem);
+  const imgUrl = URL.createObjectURL(responseAsBlob);
+  imgElem.src = imgUrl;
+}
+
+function readResponseAsBlob(response) {
+  return response.blob();
+}
+
+//fetch text
+function showText(responseAsText) {
+  const message = document.getElementById('message');
+  message.textContent = responseAsText;
+}
+
+function readResponseAsText(response) {
+  return response.text();
+}
+
+//fetch size text
+// function showSize(responseAsText) {
+//   const
+// }
